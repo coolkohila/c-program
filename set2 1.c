805ca386bad63da5ec886e68c57dc803b3af4f00
coolkohila <46813658+coolkohila@users.noreply.github.com>
2019-02-25 22:49:14 +0530

    Create set2 1.c

---
+++ set2 1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+int num,m,k;
+printf("enter  base value");
+scanf("%d",&num);
+printf("enter exponent value");
+scanf("%d",&m);
+k=pow(num,m);
+printf("the value is %d",k);
+  return 0;
+}
