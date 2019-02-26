9d5a259fcef8cb232281805f4c8b48d0bae428cf
coolkohila <46813658+coolkohila@users.noreply.github.com>
2019-02-26 09:02:41 -0800

    Create set10 92.c

---
+++ set10 92.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void) 
+{
+	int k,s[50],i,sum=0;
+	scanf("%d",&k);
+	for(i=0;i<k;i++)
+	{
+		scanf("%d",&s[i]);
+		sum=sum+s[i];	
+	}
+	printf("\n%d",sum);
+	return 0;
+}
