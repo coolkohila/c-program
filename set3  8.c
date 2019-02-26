ce1596147ce91176cff2d7acb95f617dd828a772
coolkohila <46813658+coolkohila@users.noreply.github.com>
2019-02-26 15:24:07 +0530

    Create set3  8.c

---
+++ set3  8.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() 
+{
+	int a[10],i,n;
+	printf("enter the size of the array:");
+	scanf("%d",&n);
+	printf("\n enter the array elments:");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+		printf("\n%d %d",a[i],i);
+	}
+	return 0;
+}
+	
