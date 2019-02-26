c6ead8be5b008bac781f4719c04fbdf69f21a96c
coolkohila <46813658+coolkohila@users.noreply.github.com>
2019-02-26 01:51:31 -0800

    Create set6 58.c

---
+++ set6 58.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+  {
+  int a[50],falg=0,n,k,i;
+  scanf("%d %d",&n,&k);
+  for(i=0;i<n;i++)
+  {
+  scanf("%d",&a[i]);
+  }
+  
+  for(i=0;i<n;i++)
+    {
+   if(a[i]==k)
+    {
+ falg=1;
+ }
+ 
+  }
+  
+  if(falg==1)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+return 0;
+}
