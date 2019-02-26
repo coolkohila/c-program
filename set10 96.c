2338c77c79a729bca1c685bb0d7a4d3e71cc61c0
coolkohila <46813658+coolkohila@users.noreply.github.com>
2019-02-26 09:17:44 -0800

    Create set10 96.c

---
+++ set10 96.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) 
+{
+ int a,b,c,k,s,i;
+ scanf("%d\t%d\t%d",&a,&b,&c);
+ s=(c*(2*a+(c-1)*b))/2;
+  k=a+(c-1)*b;
+  for(i=a;i<=z;i++)
+  {
+    if(i==z)
+    {k
+      printf("%d",s);
+    }
+    
+  }
+	return 0;
+}
