11c7bbde09726289a7f408edbcc4fd21370c7368
coolkohila <46813658+coolkohila@users.noreply.github.com>
2019-02-26 03:58:51 -0800

    Create set4 34.c

---
+++ set4 34.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+   int main(void) {
+   int i,k=1;
+  char s[100];
+  scanf("%[^\n]s",s);
+  for(i=0;s[i]!='\0';i++) 
+  {
+    if(s[i]=='.')
+    {
+       k++;
+    }
+    else
+    { 
+    }
+  }
+printf("%d",k);
+  	return 0;
+}
