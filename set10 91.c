79d88e0d4676e444e9707dea62fe41036e1418a5
coolkohila <46813658+coolkohila@users.noreply.github.com>
2019-02-26 08:56:35 -0800

    Create set10 91.c

---
+++ set10 91.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void) 
+{
+	int kohi,m,p;
+	scanf("%d %d %d",&kohi,&m,&p);
+	int vol,ta;
+	vol=kohi*m*p;
+	ta=2*((kohi*m)+(m*p)+(p*ko));
+	printf("\n%d",vol);
+	printf("\n%d",ta);
+  return 0;
+}
