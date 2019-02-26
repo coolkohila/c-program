cf050617e46717852d094364f3dc2838551bbeb5
coolkohila <46813658+coolkohila@users.noreply.github.com>
2019-02-26 11:05:27 -0800

    Create set9 83.c

---
+++ set9 83.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c,d;
+printf("enter the a and b values:");
+scanf("%d%d",&a,&b);
+c=a/b;
+printf("%d \n",c);
+printf("enter the a and b values:");
+scanf("%d%d",&a,&b);
+d=a%b;
+printf("%d \n",d);
+return 0;
+}
