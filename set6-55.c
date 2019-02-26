c94a184afb4d3b7b4354cb11e0ed33184e9b9b2d
coolkohila <46813658+coolkohila@users.noreply.github.com>
2019-02-26 01:30:21 -0800

    Update and rename set6-53.c to set6-55.c

---
+++ set6-55.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,m,res;
+    scanf("%d",&n);
+    scanf("%d",&m);
+    res=n*m;
+    if(res%2==0)
+{
+        printf("even");
+  }
+    else
+	    
+    {
+        printf("odd");
+    }
+    return 0;
+}
