ba073e97d528069c4c597d00e5815e5f68430675
coolkohila <46813658+coolkohila@users.noreply.github.com>
2019-02-26 05:50:59 -0800

    Create set8 75.c

---
+++ set8 75.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) 
+{
+
+	char a[30];
+	int m,i;
+	scanf("%s",a);
+	m=strlen(a);
+	if(n%2==0)
+	{
+  
+		a[m/2]='*';
+		a[(m/2)-1]='*';
+	}
+  
+	else
+	{
+		a[m/2]='*';
+	}
+	printf("\n%s",a);
+	return 0;
+}
