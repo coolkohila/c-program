bf77ef280e4a3946b554a5d2c8b436ed9fc22e37
coolkohila <46813658+coolkohila@users.noreply.github.com>
2019-02-26 20:46:51 +0530

    Create set7  8.c

---
+++ set7  8.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() 
+{
+	int no;
+	printf("enter the number:");
+	scanf("%d",&no);
+	if(no%7==0)
+	{
+		printf("\n yes");
+	}
+	else
+	{
+		printf("\n No");
+	}
+	return 0;
+}
