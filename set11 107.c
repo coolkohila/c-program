cb53e4b87a14ea36c2a2ddc506875b477707c0ba
coolkohila <46813658+coolkohila@users.noreply.github.com>
2019-02-26 10:14:37 -0800

    Create set11 107.c

---
+++ set11 107.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+
+int a,b,c;
+printf("enter the values of a,b,c");
+scanf("%d%d%d",&a,&b,&c);
+printf("answer of (a*b)/c is %d",((a*b)/c));
+
+
+    return 0;
+}
