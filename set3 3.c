56e4a7777dbd5ffd23f26a66f6c66a328f4085bb
coolkohila <46813658+coolkohila@users.noreply.github.com>
2019-02-26 13:19:33 +0530

    Create set3 3.c

---
+++ set3 3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+    int main()
+    {
+        int i, j, swap, n=3,a[30];
+        for(i=0;i<n;i++)
+        {
+            scanf("%d",&a[i]);
+        }
+        for(i=0;i<n;i++) 
+        {
+            for(j=i+1;j<n;j++)
+            {
+                if(a[i]>a[j]) 
+                {
+                    swap=a[i];
+                    a[i]=a[j];
+                    a[j]=swap;
+                }
+            }
+        }
+    printf("%d",a[n-3]);
+        return 0;
+    }
