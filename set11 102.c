c94ffa5903b4282805a134fc86e29de0f34bc323
coolkohila <46813658+coolkohila@users.noreply.github.com>
2019-02-26 09:45:25 -0800

    Create set11 102.c

---
+++ set11 102.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+    int n;
+    {
+    scanf("%d",&n);
+    while(n%2==0)
+    {
+        n=n/2;
+    }
+    if(n%2!=0)
+    {
+    printf("%d",n);
+    }
+    }
+}
