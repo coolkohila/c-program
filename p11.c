a0cae409a57c2dc2b39c121a9f168201af6ade02
coolkohila <46813658+coolkohila@users.noreply.github.com>
2019-03-10 03:11:50 -0700

    Create p11.c

---
+++ p11.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+char n[100];
+    
+scanf("%s",n);
+int i;
+ for(i=0;a[i]!='\0';i++)
+    {
+       if(n[0]=='s')
+    {
+
+  printf("yes");
+  break;
+}
+else
+    {
+        printf("no");
+    }
+}
+}
