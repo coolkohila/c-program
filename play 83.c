798acee98396ee6796223056c6f722feabe6a61c
coolkohila <46813658+coolkohila@users.noreply.github.com>
2019-03-27 03:07:47 +0530

    Create play 83.c

---
+++ play 83.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	int n,temp=0;
+	scanf("%d",&n);
+	int a[100],i;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		temp=temp+a[i];
+	}
+	printf("%d",temp);
+	return 0;
+}
