7d2f32e2d9ec9fd3f4a8e42761f638bd14d82289
coolkohila <46813658+coolkohila@users.noreply.github.com>
2019-03-27 01:47:48 +0530

    Create play 73.c

---
+++ play 73.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+ {
+  int a[100],n,i,j,m=0;
+  scanf("%d\t%d",&n,&m);
+  for(i=1;i<=n;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+  for(i=1;i<=n;i++)
+  {
+    if(a[i]==m)
+    {
+       printf("%d\t",i);
+    }
+  }
+  return 0; 
